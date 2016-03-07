665589f5647bddd27f441ed02c9a851b8da3ecb0
hotr1511 <hotr1511@gmail.com>
2016-03-06 23:46:25 -0600

    Update PruebasU.php

--- tests/PruebasU.php
+++ tests/PruebasU.php
@@ -1,17 +1,16 @@
 <?php
-class StackTest extends PHPUnit_Framework_TestCase
+class DependencyFailureTest extends PHPUnit_Framework_TestCase
 {
-    public function testPushAndPop()
+    public function testOne()
     {
-        $stack = array();
-        $this->assertEquals(0, count($stack));
+        $this->assertTrue(FALSE);
+    }
 
-        array_push($stack, 'foo');
-        $this->assertEquals('foo', $stack[count($stack)-1]);
-        $this->assertEquals(1, count($stack));
-
-        $this->assertEquals('foo', array_pop($stack));
-        $this->assertEquals(0, count($stack));
+    /**
+     * @depends testOne
+     */
+    public function testTwo()
+    {
     }
 }
 ?>
