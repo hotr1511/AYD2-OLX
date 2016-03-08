fdb687d252b498e21f719034f4a25003afb6bd33
bjesua <jesuasagas@gmail.com>
2016-03-08 08:10:55 -0600

    up

--- src/upload/olx_upload.php
+++ src/upload/olx_upload.php
@@ -6,7 +6,7 @@
 	 pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION), isset($_POST["submit"]),
 			$_FILES["fileToUpload"]["tmp_name"], $_POST["title"], $_POST["description"]);
 	if($status){
-		echo 'El fichero ' . $_FILES["fileToUpload"]["name"] . ' se ha subido con exito.';
+		echo 'El el fichero ' . $_FILES["fileToUpload"]["name"] . ' se ha subido con exito.';
 	} else {
 		echo 'Error al subir el fichero.';
 	}
